998846ae5a8d50f6590a696a6caccb960f19f091
Chermnykh Daria <83666036+Chermnykh@users.noreply.github.com>
2022-10-19 15:22:49 +0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,57 +1,82 @@
-# import cv2
-# import os
-import numpy as np
-# from skimage.filters import gaussian
-import streamlit as st
-from PIL import Image
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tempfile import NamedTemporaryFile
+import streamlit as st          # lerkvln
+from PIL import Image               # lsmb
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import tensorflow as tf         # lemvlm
+import numpy as np              # vkm
+# from tensorflow import keras
+from tensorflow.keras.models import load_model          # hello
+from tensorflow.keras import preprocessing
+import time
+
+# from tempfile import NamedTemporaryFile  # lrmve
+
+fig = plt.figure()
+
+with open("custom.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.title('Emotion Classifier')
+
+st.markdown(
+    "Welcome to this simple web application that classifies your emotion. Emotions are classified into two classes namely: Happy, Sad.")
 
 
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png", "jpg", "jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
-model = load_model('model.h5')
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                plt.imshow(image)
+                plt.axis("off")
+                predictions = predict(image)
+                time.sleep(1)
+                st.success('Classified')
+                st.write(predictions)
+                st.pyplot(fig)
 
 
-st.title('Emotions Classifier')
+def predict(image):
+    classifier_model = "model.h5"
+#     IMAGE_SHAPE = (224, 224, 3)
+    model = load_model(classifier_model, compile=False, custom_objects={'KerasLayer': hub.KerasLayer})
+    test_pic = image.resize((150, 150))
+    test_pic = tf.keras.preprocessing.image.img_to_array(test_pic)
+    test_pic = test_pic/255 # normalization
+    test_pic = np.expand_dims(test_pic, axis=0)
+    prediction = model.predict(test_pic)
+    print(prediction)
 
-st.sidebar.title("Emotions Classifier")
-st.sidebar.subheader('CNN + Binary Classification')
+    if prediction[0] < 0.5:
+        st.markdown("Prediction: Happy")
+    else:
+        st.markdown("Prediction: Sad")
+
+#     result = f"{class_names[np.argmax(scores)]} with a {(100 * np.max(scores)).round(2)} % confidence."
+#     return result
 
 
-image_file_buffer = st.sidebar.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
+if __name__ == "__main__":
+    main()
 
+# #predict
+# test_pic = tf.keras.preprocessing.image.load_img(demo_image)
+# test_pic = test_pic.resize((150, 150))
+# test_pic = tf.keras.preprocessing.image.img_to_array(test_pic)
+# test_pic = test_pic/255 # normalization
+# test_pic = np.expand_dims(test_pic, axis=0)
+# prediction = model.predict(test_pic)
+# print(prediction)
 
-# If file uploader is used
-if image_file_buffer is not None:
-    image = np.array(Image.open(image_file_buffer))
-    demo_image = image_file_buffer
-    # display image
-    st.image(demo_image, use_column_width=True)
-
-    temp_file = NamedTemporaryFile(delete=False)
-    temp_file.write(image_file_buffer.getvalue())
-    demo_image = temp_file.name
-
-# set a default image when no image is uploaded
-else:
-    demo_image = "Validation/5.jpg"
-    # image = np.array(Image.open(demo_image))
-
-    # display image
-    st.image(demo_image, use_column_width=True)
-
-#predict
-test_pic = tf.keras.preprocessing.image.load_img(demo_image)
-test_pic = test_pic.resize((150, 150))
-test_pic = tf.keras.preprocessing.image.img_to_array(test_pic)
-test_pic = test_pic/255 # normalization
-test_pic = np.expand_dims(test_pic, axis=0)
-prediction = model.predict(test_pic)
-print(prediction)
-
-if prediction[0] < 0.5:
-    st.markdown("Prediction: Happy")
-else:
-    st.markdown("Prediction: Sad")
+# if prediction[0] < 0.5:
+#     st.markdown("Prediction: Happy")
+# else:
+#     st.markdown("Prediction: Sad")
 
